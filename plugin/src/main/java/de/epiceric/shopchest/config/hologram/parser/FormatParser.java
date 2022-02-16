72e04ba07a851ce1784faeeb89b6b88bed04444d
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-02-16 18:12:21 +0100

    Add token parser

---
+++ plugin/src/main/java/de/epiceric/shopchest/config/hologram/parser/FormatParser.java
@@ -0,0 +1,155 @@
+package de.epiceric.shopchest.config.hologram.parser;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FormatParser {
+
+    public List<Token<?>> getTokens(String string) {
+        final List<Token<?>> tokens = new LinkedList<>();
+
+        final char[] chars = string.toCharArray();
+
+        StringBuilder currentString = null;
+        StringBuilder currentToken = new StringBuilder();
+
+        for (int index = 0; index < chars.length; index++) {
+            final char currentChar = chars[index];
+
+            // String detection
+            if (currentChar == '\"') {
+                if (currentString != null) {
+                    tokens.add(new Token<>(Token.STRING, currentString.toString()));
+                    currentString = null;
+                } else {
+                    currentString = new StringBuilder();
+                }
+                continue;
+            }
+
+            // Add the char to the current string
+            if (currentString != null) {
+                currentString.append(currentChar);
+            }
+
+            // Reverse detection
+            if (currentChar == '!' && currentToken.length() == 0 && index + 1 < chars.length && chars[index + 1] != '=') {
+                tokens.add(new Token<>(Token.REVERSE, null));
+                continue;
+            }
+
+            final boolean isSpace = currentChar == ' ';
+            final boolean isOpenParenthesis = currentChar == '(';
+            final boolean isCloseParenthesis = currentChar == ')';
+            final boolean isLastChar = index + 1 >= chars.length;
+
+            // Handle specific token
+            if (
+                    isSpace ||
+                            isOpenParenthesis ||
+                            isCloseParenthesis ||
+                            isLastChar
+            ) {
+                if (isLastChar) {
+                    // Add the last char only if it's the last (otherwise it will be skipped)
+                    currentToken.append(currentChar);
+                }
+
+                // If it's empty, don't need to add a token
+                if (currentToken.length() == 0) {
+                    continue;
+                }
+
+                final String stringToken = currentToken.toString();
+
+                // Double detection
+                try {
+                    final double doubleValue = Double.parseDouble(stringToken);
+                    tokens.add(new Token<>(Token.DOUBLE, doubleValue));
+
+                    // Duplicate due to the double detection with exception
+
+                    // Reset the specific token
+                    currentToken = new StringBuilder();
+
+                    // Unit detection
+                    if (isOpenParenthesis) {
+                        tokens.add(new Token<>(Token.BEGIN_UNIT, null));
+                    } else if (isCloseParenthesis) {
+                        tokens.add(new Token<>(Token.END_UNIT, null));
+                    }
+                    continue;
+                } catch (Exception ignored) {
+                }
+
+                // Operator detection
+                final Token<?> token;
+                switch (stringToken) {
+                    // Calculation
+                    case "+":
+                        token = new Token<>(Token.CALCULATION_OPERATOR, Token.CalculationOperator.ADDITION);
+                        break;
+                    case "-":
+                        token = new Token<>(Token.CALCULATION_OPERATOR, Token.CalculationOperator.SUBTRACTION);
+                        break;
+                    case "*":
+                        token = new Token<>(Token.CALCULATION_OPERATOR, Token.CalculationOperator.MULTIPLICATION);
+                        break;
+                    case "/":
+                        token = new Token<>(Token.CALCULATION_OPERATOR, Token.CalculationOperator.DIVISION);
+                        break;
+                    case "%":
+                        token = new Token<>(Token.CALCULATION_OPERATOR, Token.CalculationOperator.MODULO);
+                        break;
+                    // Logic
+                    case "&&":
+                        token = new Token<>(Token.LOGIC_OPERATOR, Token.LogicOperator.AND);
+                        break;
+                    case "||":
+                        token = new Token<>(Token.LOGIC_OPERATOR, Token.LogicOperator.OR);
+                        break;
+                    // Condition
+                    case "==":
+                        token = new Token<>(Token.CONDITION_OPERATOR, Token.ConditionOperator.EQUAL);
+                        break;
+                    case "!=":
+                        token = new Token<>(Token.CONDITION_OPERATOR, Token.ConditionOperator.NOT_EQUAL);
+                        break;
+                    case ">":
+                        token = new Token<>(Token.CONDITION_OPERATOR, Token.ConditionOperator.GREATER);
+                        break;
+                    case "<":
+                        token = new Token<>(Token.CONDITION_OPERATOR, Token.ConditionOperator.LESS);
+                        break;
+                    case ">=":
+                        token = new Token<>(Token.CONDITION_OPERATOR, Token.ConditionOperator.GREATER_OR_EQUAL);
+                        break;
+                    case "<=":
+                        token = new Token<>(Token.CONDITION_OPERATOR, Token.ConditionOperator.LESS_OR_EQUAL);
+                        break;
+                    // Boolean
+                    default:
+                        token = new Token<>(Token.BOOLEAN, stringToken);
+                }
+
+                tokens.add(token);
+
+                // Reset the specific token
+                currentToken = new StringBuilder();
+
+                // Unit detection
+                if (isOpenParenthesis) {
+                    tokens.add(new Token<>(Token.BEGIN_UNIT, null));
+                } else if (isCloseParenthesis) {
+                    tokens.add(new Token<>(Token.END_UNIT, null));
+                }
+                continue;
+            }
+
+            // Add the char to currentToken to handle specific token
+            currentToken.append(currentChar);
+        }
+        return tokens;
+    }
+
+}
---
+++ plugin/src/main/java/de/epiceric/shopchest/config/hologram/parser/Token.java
@@ -0,0 +1,69 @@
+package de.epiceric.shopchest.config.hologram.parser;
+
+public class Token<T> {
+
+    public final static TokenType<String> STRING = new TokenType<>("String");
+    public final static TokenType<Double> DOUBLE = new TokenType<>("Double");
+    public final static TokenType<String> BOOLEAN = new TokenType<>("Boolean");
+    public final static TokenType<Void> BEGIN_UNIT = new TokenType<>("Begin unit");
+    public final static TokenType<Void> END_UNIT = new TokenType<>("End unit");
+    public final static TokenType<Void> REVERSE = new TokenType<>("Reverse");
+    public final static TokenType<ConditionOperator> CONDITION_OPERATOR = new TokenType<>("Condition operator");
+    public final static TokenType<LogicOperator> LOGIC_OPERATOR = new TokenType<>("Logic operator");
+    public final static TokenType<CalculationOperator> CALCULATION_OPERATOR = new TokenType<>("Calculation operator");
+    private final TokenType<T> type;
+    private final T value;
+
+    public Token(TokenType<T> type, T value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public TokenType<T> getType() {
+        return type;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "type=" + type +
+                ", value=" + value +
+                '}';
+    }
+
+    public enum ConditionOperator {
+        EQUAL, NOT_EQUAL, GREATER, GREATER_OR_EQUAL, LESS, LESS_OR_EQUAL
+    }
+
+    public enum LogicOperator {
+        OR, AND
+    }
+
+    public enum CalculationOperator {
+        ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, MODULO
+    }
+
+    public final static class TokenType<T> {
+
+        private final String id;
+
+        public TokenType(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public String toString() {
+            return "TokenType{" +
+                    "id='" + id + '\'' +
+                    '}';
+        }
+    }
+}
