e4c577c626280c14447286c3971667902ffd7fe2
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-08-21 16:01:38 +0200

    Add 1.19.2 support #9

--- plugin/src/main/java/de/epiceric/shopchest/nms/PlatformLoader.java
+++ plugin/src/main/java/de/epiceric/shopchest/nms/PlatformLoader.java
@@ -83,7 +83,8 @@ public class PlatformLoader {
             case "eaeedbff51b16ead3170906872fda334": // 1.18.2 (v1_18_R2)
                 return new de.epiceric.shopchest.nms.v1_18_R2.PlatformImpl();
             case "7b9de0da1357e5b251eddde9aa762916": // 1.19
-            case "4cc0cc97cac491651bff3af8b124a214": // 1.19.1 (v1_19_R1)
+            case "4cc0cc97cac491651bff3af8b124a214": // 1.19.1
+            case "69c84c88aeb92ce9fa9525438b93f4fe": // 1.19.2 (v1_19_R1)
                 return new de.epiceric.shopchest.nms.v1_19_R1.PlatformImpl();
             default:
                 return null;
